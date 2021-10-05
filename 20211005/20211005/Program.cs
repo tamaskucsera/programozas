3445ba3ab0b896e5509261352347c6343a28d3ac
Kucsera Tamás <90682235+tamaskucsera@users.noreply.github.com>
2021-10-05 11:31:11 +0200

    ciklus202110051130_02
    Órai munka első 9 feladat

--- 20211005/20211005/Program.cs
+++ 20211005/20211005/Program.cs
@@ -10,11 +10,19 @@ namespace _20211005
     {
         static void Main(string[] args)
         {
-            
-            feladat1();
-            feladat2();
-            feladat3();
-                Console.WriteLine("Lépjen ki! A program már nem üzemképes!");
+
+            //feladat1();
+            //feladat2();
+            //feladat3();
+            //feladat4();
+            //feladat5();
+            //feladat6();
+            //feladat7();
+            //feladat8();
+            feladat9();
+
+
+            Console.WriteLine("Lépjen ki! A program már nem üzemképes!");
                 Console.ReadLine();
             
 
@@ -52,6 +60,96 @@ namespace _20211005
         static void feladat3()
         {
             Console.WriteLine("Ez a program kiíratja a számokat 15-92 között egymás mellé.");
+            int i = 15;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 93);
+        }
+        static void feladat4()
+        {
+            Console.WriteLine("Ez a program kiírja 1-30 között a páros számokat.");
+            int parostalan = 0;
+                do
+            {
+                if (parostalan % 2 == 0)
+                {
+                    Console.Write("{0} ",parostalan);
+                }
+                parostalan++;
+            }
+            while (parostalan < 31);
+            Console.WriteLine();
+
+            for (int i = 0; i < 31; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.WriteLine();
+        }
+        static void feladat5()
+        {
+            Console.WriteLine("Adj meg egy +E számot és kiírjuk az előtte lévő +E számokat");
+            int szam = int.Parse(Console.ReadLine());
+            for (int otos = 0; otos < szam; otos++)
+            {  
+                    Console.Write("{0} ", otos);
+            }
+            Console.WriteLine();
+
+        }
+        static void feladat6()
+        {
+            Console.WriteLine("Kiírjuk az első 15 szám négzetét");
+            for (int szam = 1; szam < 16; szam++)
+            {
+                Console.WriteLine("A(z) {0} négyzete: {1}",szam, Math.Pow(szam, 2));
+            }
+            Console.WriteLine();
+        }
+        static void feladat7()
+        {
+            Console.WriteLine("Ez a program kiírja a 4-gyel osztható számokat 100-400 között.");
+            int oszthatosag = 100;
+            do
+            {
+                if (oszthatosag % 4 == 0)
+                {
+                    Console.Write("{0} ", oszthatosag);
+                }
+                oszthatosag++;
+            }
+            while (oszthatosag < 401);
+            Console.WriteLine();
+        }
+        static void feladat8()
+        {
+            Console.WriteLine("Ez a program kiírja 9-esével a 30-100 között lévő számokat.");
+            int kilencesevel = 30;
+            do
+            {
+                Console.Write("{0} ", kilencesevel);
+                kilencesevel = kilencesevel + 9;
+            }
+            while (kilencesevel < 101);
+            Console.WriteLine();
+        }
+        static void feladat9()
+        {
+            Console.WriteLine("Ez a varázslatosan szép prg kiírja 120-40 köüzött a számokat 12-esével");
+            int tizenketto = 120;
+            do
+            {
+                Console.Write("{0} ", tizenketto);
+                tizenketto = tizenketto - 12;
+            }
+            while (tizenketto > 40);
+            Console.WriteLine();
         }
     }
 }
