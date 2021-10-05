fca295c67dfb5f1e63a90ba600dff8cb14c6f25e
Kucsera Tamás <90682235+tamaskucsera@users.noreply.github.com>
2021-10-05 08:44:56 +0200

    202010050844
    Órai munka

---
+++ 20211005/20211005/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            feladat1();
+            feladat2();
+            feladat3();
+                Console.WriteLine("Lépjen ki! A program már nem üzemképes!");
+                Console.ReadLine();
+            
+
+            
+        }
+        static void feladat1()
+        {
+            Console.WriteLine("Ez a program kiíratja a számokat 1-20 között egymás mellé.");
+            int[] szamok = new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                szamok[i] = i + 1;
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("{0} ",szamok[i]);
+            }
+            Console.WriteLine("\nEnter");
+            Console.ReadKey();
+        }
+        static void feladat2()
+        {
+            Console.WriteLine("Ez a program kiíratja a számokat 1-20 között egymás alá.");
+            int[] szamok = new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                szamok[i] = i + 1;
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(szamok[i]);
+            }
+            Console.ReadKey();
+        }
+        static void feladat3()
+        {
+            Console.WriteLine("Ez a program kiíratja a számokat 15-92 között egymás mellé.");
+        }
+    }
+}
